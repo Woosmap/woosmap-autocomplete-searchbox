39e11274dd0a9a0c7f8bbcbeda5bdbfd411c6838
gaelsimon <gsimon@webgeoservices.com>
2017-12-18 14:25:49 +0100

    Pushed a defaultconfig with default values

---
+++ app/src/defaultconfig.js
@@ -0,0 +1,21 @@
+module.exports = {
+    google: {
+        clientId: '',
+        apiKey: '',
+        channel: '',
+        librariesToLoad: ['places'],
+        language: 'fr',
+        region: 'FR',
+        version: '3'
+    },
+    woosmap: {
+        projectKey: 'woos-0c78592f-13ea-362b-aa07-ba4ba9ea3dae',
+        storesByPage: 5,
+        maxDistance: 0
+    },
+    autocomplete: {
+        minChars: 2,
+        maxItems: 10,
+        autoFirst: true
+    }
+};
