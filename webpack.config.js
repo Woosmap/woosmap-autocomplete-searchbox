a791c196391f8960c6202c6bf5f751f1a5f8a5bd
gaelsimon <gsimon@webgeoservices.com>
2017-12-18 17:02:47 +0100

    Fixed Webpack CSS concat

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,16 @@
 "use strict";
 var webpack = require('webpack');
+var CopyWebpackPlugin = require('copy-webpack-plugin');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 var webpackConfig = {
-    node: {
-        fs: "empty"
-    },
     entry: ['./app/src/init.js', './app/css/autocomplete.base.css', './app/css/autocomplete.theme.css'],
 
-    output: {path: __dirname + '/build', filename: "autocompletewoosmap.js", publicPath: "./"},
+    output: {path: __dirname + '/build', filename: "autocompletewoosmap.js", publicPath: "/"},
 
     module: {
-        rules: [
-            {
-                test: /\.js$/,
-                loader: "eslint-loader",
-                exclude: /node_modules/
-            },
-            {
-                test: /\.css$/,
-                exclude: /node_modules/,
-                loader: ExtractTextPlugin.extract({
-                    fallback: "style-loader",
-                    use: "css-loader"
-                })
-            }
-        ]
+        loaders: []
     },
 
     devServer: {
@@ -35,11 +19,22 @@ var webpackConfig = {
 
     plugins: [
         new HtmlWebpackPlugin({
-            template: 'sample/index.html'
+            template: 'sample/index.html',
+            inject: 'head'
         }),
         new ExtractTextPlugin("autocompletewoosmap.css")
     ]
 };
+webpackConfig.module.loaders.push({
+    test: /\.js$/,
+    loader: "eslint-loader",
+    exclude: /node_modules/
+});
+webpackConfig.module.loaders.push({
+    test: /\.css$/,
+    exclude: /node_modules/,
+    loader: ExtractTextPlugin.extract({fallback: "style-loader", use: "css-loader"})
+});
 
 if (process.env.DEBUG === 'yes') {
     webpackConfig.devtool = 'source-map';
